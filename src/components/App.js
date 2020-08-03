854dd4df73027d1b682d296ecaa4717a4b04e4ab
Ismael Arias <ismaeljarias@gmail.com>
2020-08-03 07:57:49 -0400

    Optimizing some code, using custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,27 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
-
-import youtube from "../apis/youtube";
 import VideoDetail from "./VideoDetail";
-
-const KEY = process.env.REACT_APP_YOUTUBE_API;
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("One For All: Shoot Style!");
 
   useEffect(() => {
-    onTermSubmit("Midorita save almighty");
-  }, []);
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-        part: "snippet",
-        type: "video",
-        maxResults: 5,
-        key: KEY,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onTermSubmit={onTermSubmit} videos={videos} />
+      <SearchBar onTermSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
